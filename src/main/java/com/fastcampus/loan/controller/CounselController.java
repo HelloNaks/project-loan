ea8fe171e5cf697cf6f1e63f3b91c3d29305d88e
김낙현 <rakhyun_kim2@tmax.co.kr>
2025-01-19 18:17:56 +0900

    [feat] delete loan counsel

--- src/main/java/com/fastcampus/loan/controller/CounselController.java
+++ src/main/java/com/fastcampus/loan/controller/CounselController.java
@@ -27,4 +27,10 @@ public class CounselController extends AbstractController{
     public ResponseDTO<CounselDto.Response> update(@PathVariable Long counselId, @RequestBody CounselDto.Request request) {
         return ok(counselService.update(counselId, request));
     }
+
+    @DeleteMapping("/{counselId}")
+    public ResponseDTO<CounselDto.Response> delete(@PathVariable Long counselId) {
+        counselService.delete(counselId);
+        return ok();
+    }
 }
